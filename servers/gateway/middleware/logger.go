f99387fad6d8a1fc40865e3a5629d5a0f846b768
Brendan Kellogg <brendan6@uw.edu>
2017-11-14 10:03:05 -0800

    fixed email validation; comments for logger

--- servers/gateway/middleware/logger.go
+++ servers/gateway/middleware/logger.go
@@ -8,31 +8,41 @@ import (
 	"github.com/BKellogg/UWDanceCapstone/servers/gateway/models"
 )
 
+// loggingResponseWriter defines a wrapper for an http.ResponseWriter
+// that saves status code and the message for an http response
 type loggingResponseWriter struct {
 	http.ResponseWriter
 	statusCode int
 	response   string
 }
 
+// WriteHeader saves the given status code and writes the status
+// code to the wrapped http.ResponseWriter
 func (lrw *loggingResponseWriter) WriteHeader(statusCode int) {
 	lrw.statusCode = statusCode
 	lrw.ResponseWriter.WriteHeader(statusCode)
 }
 
+// Write saves the given content []byte slice and writes it
+// to the wrapped http.ResponseWriter
 func (lrw *loggingResponseWriter) Write(content []byte) (int, error) {
 	lrw.response = string(content)
 	return lrw.ResponseWriter.Write(content)
 }
 
+// Logger defines the infromation needed for logging middleware
 type Logger struct {
 	handler http.Handler
 	db      *models.Database
 }
 
+// NewLogger returns a pointer to a Logger from the given information
 func NewLogger(handler http.Handler, db *models.Database) *Logger {
 	return &Logger{handler: handler, db: db}
 }
 
+// ServeHTTP wraps the Logger's handlers ServeHTTP function and records
+// various information about the request
 func (l *Logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	startTime := time.Now()
 	lrw := &loggingResponseWriter{w, http.StatusOK, ""}
--- servers/gateway/models/newUserRequest.go
+++ servers/gateway/models/newUserRequest.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"errors"
+	"net/mail"
 
 	"github.com/BKellogg/UWDanceCapstone/servers/gateway/constants"
 )
@@ -41,8 +42,8 @@ func (u *NewUserRequest) isReadyForUser() error {
 	if len(u.LastName) == 0 {
 		return errors.New("new user last name must exist")
 	}
-	if len(u.Email) == 0 {
-		return errors.New("new user email must exist")
+	if _, err := mail.ParseAddress(u.Email); err != nil {
+		return errors.New("new users must supply a valid email address")
 	}
 	if len(u.Password) < 6 {
 		return errors.New("new users must have a password")
