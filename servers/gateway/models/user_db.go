b79419b449ff9f0b39ac729ac48f91ca5ebdc8af
Brendan Kellogg <brendan6@uw.edu>
2017-12-17 14:41:00 -0800

    renames

--- servers/gateway/models/user_db.go
+++ servers/gateway/models/user_db.go
@@ -2,36 +2,9 @@ package models
 
 import (
 	"database/sql"
-	"errors"
-	"log"
 
 	"github.com/BKellogg/UWDanceCapstone/servers/gateway/constants"
-	"github.com/go-sql-driver/mysql"
 )
-
-// Database defines a sql database struct
-type Database struct {
-	DB *sql.DB
-}
-
-// NewDatabase returns a new Database with an open sql DB connection from the given
-// connection information. Returns an error if it failed to open the connection, or
-// if it fails to ping the DB after three tries
-func NewDatabase(user, password, addr, dbName string) (*Database, error) {
-	connectionInfo := mysql.Config{
-		User:      user,
-		Passwd:    password,
-		Net:       "tcp",
-		Addr:      addr,
-		DBName:    dbName,
-		ParseTime: true,
-	}
-	db, err := sql.Open("mysql", connectionInfo.FormatDSN())
-	if err != nil {
-		return nil, errors.New("error opening connection to Database: " + err.Error())
-	}
-	return &Database{DB: db}, nil
-}
 
 // GetAllUsers returns a slice of users of every user in the database, active or not.
 // Returns an error if one occured
@@ -182,13 +155,4 @@ func (store *Database) DeactivateUserByID(userID int) error {
 func (store *Database) ActivateUserByID(userID int) error {
 	_, err := store.DB.Exec(`UPDATE Users SET Active = ? WHERE UserID = ?`, constants.UserActive, userID)
 	return err
-}
-
-// LogError logs the given code and message to the database
-func (store *Database) LogError(err ErrorLog) {
-	_, dbErr := store.DB.Exec(`INSERT INTO Errors (ErrTime, ErrRemoteAddr, ErrRequestMethod, ErrRequestURI, ErrCode, ErrMessage) VALUES (?, ?, ?, ?, ?, ?)`,
-		err.Time, err.RemoteAddr, err.RequestMethod, err.RequestURI, err.Code, err.Message)
-	if dbErr != nil {
-		log.Println("logging failed: " + dbErr.Error())
-	}
 }
