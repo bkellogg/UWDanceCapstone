ba49cab65060170a66a033feb6aa2d817b5c3741
Brendan Kellogg <brendan6@uw.edu>
2018-01-12 11:02:36 -0800

    basic working auth/create functionality

---
+++ clients/console/js/common.js
@@ -0,0 +1,74 @@
+// Contains functionality needed between many files. Include before including page specific
+// js.
+
+"use strict";
+
+const headerAuthorization = "Authorization";
+
+const API_URL_BASE = "https://dasc.capstone.ischool.uw.edu/api/v1/";
+
+function saveAuth(auth) {
+    localStorage.setItem("auth", auth);
+}
+
+function setLocalUser(user) {
+    localStorage.setItem("user", user);
+}
+
+function getLocalUser() {
+    return JSON.parse(localStorage.getItem("user"));
+}
+
+function getAuth() {
+    return localStorage.getItem("auth");
+}
+
+function clearAuthAndUser() {
+    localStorage.removeItem("auth");
+    localStorage.removeItem("user");
+}
+
+function makeRequest(resource, payload = "", method = "GET", useAuth = false) {
+    var reqURL = API_URL_BASE + resource;
+    payload = JSON.stringify(payload)
+    var headers = new Headers();
+    if (useAuth) {
+        headers.append(headerAuthorization, getAuth())
+    }
+    headers.append("Content-Type", "application/json");
+    headers.append("Content-Length", payload.length.toString())
+    var req = {
+        method: method,
+        headers: headers
+    }
+    if (method != "GET") {
+        req.body = payload;
+    }
+    return fetch(reqURL, req);
+}
+
+function refreshLocalUser() {
+    return makeRequest("users/me", {}, "GET", true)
+    .then((res) => {
+        if (res.ok) {
+            return res.json();
+        }
+        return res.text().then((t) => Promise.reject(t));
+    })
+    .then((data) => {
+        setLocalUser(JSON.stringify(data));
+    })
+    .catch((err) => {
+        console.error(err);
+        alert(err);
+        signout();
+    })
+}
+
+function signout() {
+    makeRequest("sessions", {}, "DELETE", true);
+    // no need to handle the respose here. If it fails, you can treat the auth and
+    // user as invalid and clear their local storage entries anyway.
+    clearAuthAndUser();
+    window.location.href = "/index.html";
+}
---
+++ clients/console/js/console.js
@@ -0,0 +1,123 @@
+"use strict";
+
+var content = document.querySelector(".content");
+
+var createAuditionButton = document.querySelector(".js-create-audition");
+var createAuditionForm = document.querySelector(".create-audition-form");
+
+var createShowButton = document.querySelector(".js-create-show");
+var createShowForm = document.querySelector(".create-show-form");
+
+var createPieceButton = document.querySelector(".js-create-piece");
+var createPieceForm = document.querySelector(".create-piece-form");
+
+var signoutButton = document.querySelector(".js-signout-btn");
+var refreshUserButton = document.querySelector(".js-refresh-user-btn");
+
+var welcome = document.createElement("p");
+var user = {};
+
+var auditionFormWrapper = $('.create-audition-form-wrapper');
+auditionFormWrapper.hide();
+var showFormWrapper = $('.create-show-form-wrapper');
+showFormWrapper.hide();
+var pieceFormWrapper = $('.create-piece-form-wrapper');
+pieceFormWrapper.hide();
+
+var auditionForm = $('.create-audition-form');
+
+refreshLocalUser().then(() => {
+    user = getLocalUser();
+    welcome.innerText = "Welcome " + user.firstName + "! Your current access level is: " + user.role + ".";
+    content.appendChild(welcome);
+});
+
+signoutButton.addEventListener("click", signout);
+refreshUserButton.addEventListener("click", () => {
+    refreshLocalUser().then(() => {
+        user = getLocalUser();
+        welcome.innerText = "Welcome " + user.firstName + "! Your current access level is: " + user.role + ".";
+    })
+});
+
+createAuditionButton.addEventListener("click", () => {
+    auditionFormWrapper.toggle();
+});
+createShowButton.addEventListener("click", () => {
+    showFormWrapper.toggle();
+});
+createPieceButton.addEventListener("click", () => {
+    pieceFormWrapper.toggle();
+});
+createAuditionForm.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    var resBox = $('.create-aud-res-box');
+    var payload = {
+        "name": $('#aud-name-input').val(),
+        "date": $('#aud-date-input').val(),
+        "time": $('#aud-time-input').val(),
+        "location": $('#aud-location-input').val(),
+        "quarter": $('#aud-quarter-input').val(),
+        "year": parseInt($('#aud-year-input').val())
+    };
+    makeRequest("auditions", payload, "POST", true)
+    .then((res) => {
+        if (res.ok) {
+            return res.json();
+        } else {
+            return res.text().then((t) => Promise.reject(t));
+        }
+    })
+    .then((data) => {
+        resBox.text("Success! The new audition's ID is: " + data.id);
+    })
+    .catch((err) => {
+        resBox.text("Failed to create audition: " + err);
+    })
+});
+
+createShowForm.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    var resBox = $('.create-show-res-box');
+    var payload = {
+        "name": $('#show-name-input').val(),
+        "auditionid": parseInt($('#show-audid-input').val())
+    };
+    makeRequest("shows", payload, "POST", true)
+    .then((res) => {
+        if (res.ok) {
+            return res.json();
+        } else {
+            return res.text().then((t) => Promise.reject(t));
+        }
+    })
+    .then((data) => {
+        resBox.text("Success! The new show's ID is: " + data.id);
+    })
+    .catch((err) => {
+        resBox.text("Failed to create show: " + err);
+    })
+});
+
+createPieceForm.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    var resBox = $('.create-piece-res-box');
+    var payload = {
+        "name": $('#piece-name-input').val(),
+        "showID": parseInt($('#piece-showid-input').val())
+    };
+    makeRequest("pieces", payload, "POST", true)
+    .then((res) => {
+        if (res.ok) {
+            return res.json();
+        } else {
+            return res.text().then((t) => Promise.reject(t));
+        }
+    })
+    .then((data) => {
+        resBox.text("Success! The new show's ID is: " + data.id);
+    })
+    .catch((err) => {
+        resBox.text("Failed to create show: " + err);
+    })
+});
