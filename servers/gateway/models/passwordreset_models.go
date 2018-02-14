a34a19150bb70351de5be11028d3c0b7ddeab3bb
Brendan Kellogg <brendan6@uw.edu>
2018-02-14 01:34:40 -0800

    typo

--- servers/gateway/models/passwordreset_models.go
+++ servers/gateway/models/passwordreset_models.go
@@ -2,6 +2,6 @@ package models
 
 type PasswordResetRequest struct {
 	Token        string `json:"token"`
-	Password     sting  `json:"password"`
+	Password     string `json:"password"`
 	PasswordConf string `json:"passwordConf"`
 }
