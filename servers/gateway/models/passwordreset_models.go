5e131b9f1d8449af8ca5590fecbdfe0c9715a008
Brendan Kellogg <brendan6@uw.edu>
2018-02-14 13:57:54 -0800

    working password reset

--- servers/gateway/models/passwordreset_models.go
+++ servers/gateway/models/passwordreset_models.go
@@ -1,5 +1,7 @@
 package models
 
+// PasswordResetRequest represents how a password reset
+// confirmation request is sent tio the server.
 type PasswordResetRequest struct {
 	Token        string `json:"token"`
 	Password     string `json:"password"`
--- servers/gateway/sessions/passwordreset.go
+++ servers/gateway/sessions/passwordreset.go
@@ -1,10 +1,13 @@
 package sessions
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"errors"
+	"fmt"
 	"github.com/BKellogg/UWDanceCapstone/servers/gateway/constants"
-	"math/rand"
-	"strconv"
+	"gopkg.in/redis.v5"
+	"strings"
 )
 
 // passwordResetPrefix is a prefix that will be used for all keys stored
@@ -18,10 +21,13 @@ func (rs *RedisStore) NewPasswordResetToken(email string) (string, error) {
 	if len(email) == 0 {
 		return "", errors.New("email cannot be 0 length")
 	}
-	token := ""
-	for i := 0; i < 30; i++ {
-		token += strconv.Itoa(rand.Intn(10))
+	randomBytes := make([]byte, constants.PasswordResetTokenLength)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return "", errors.New("error generating password reset token")
 	}
+
+	token := base64.StdEncoding.EncodeToString(randomBytes)
 
 	if err := rs.Client.Set(passwordResetPrefix+email,
 		token, constants.PasswordResetValidityDuration).Err(); err != nil {
@@ -32,15 +38,34 @@ func (rs *RedisStore) NewPasswordResetToken(email string) (string, error) {
 
 // ValidatePasswordResetToken validates that the given token exists for the given
 // email. Returns an error if no such token exists, or there was an error validating it.
-func (rs *RedisStore) ValidatePasswordResetToken(email, token string) error {
+func (rs *RedisStore) ValidatePasswordResetToken(email, token string) (bool, error) {
 	cmd := rs.Client.Get(passwordResetPrefix + email)
 	err := cmd.Err()
 	if err != nil {
-		return err
+		if err == redis.Nil {
+			return false, errors.New(constants.ErrPasswordResetTokensMismatch)
+		}
+		return false, err
 	}
-	storedToken := cmd.String()
+	storedTokenBytes, _ := cmd.Bytes()
+	storedToken := string(storedTokenBytes)
+	fmt.Println("stored token: " + storedToken)
+	storedToken = strings.TrimPrefix(storedToken, passwordResetPrefix)
 	if token != storedToken {
-		return errors.New(constants.ErrPasswordResetTokensMismatch)
+		return false, errors.New(constants.ErrPasswordResetTokensMismatch)
 	}
-	return nil
+	rs.Client.Del(passwordResetPrefix + email)
+	return true, nil
+}
+
+// ValidatePasswords returns true if the password and passwordConf
+// combination are valid, false and an error if otherwise.
+func ValidatePasswords(password, passwordConf string) (bool, error) {
+	if len(password) < constants.PasswordMinLength {
+		return false, errors.New(constants.ErrPasswordNotLongEnough)
+	}
+	if password != passwordConf {
+		return false, errors.New(constants.ErrPasswordsMismatch)
+	}
+	return true, nil
 }
