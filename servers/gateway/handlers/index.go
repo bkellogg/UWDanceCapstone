2ba76fcab64ac7e3ec063fbdafc6ca179c3083c5
Brendan Kellogg <brendan6@uw.edu>
2018-02-24 21:24:38 -0800

    deploy for front end

---
+++ servers/gateway/handlers/index.go
@@ -0,0 +1,11 @@
+package handlers
+
+import "net/http"
+
+func IndexHandler(entryPoint string) func(w http.ResponseWriter, r *http.Request) {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, entryPoint)
+	}
+
+	return http.HandlerFunc(fn)
+}
