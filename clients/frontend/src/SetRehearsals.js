a03435d6292e55e80532b5b565ce9800181435e4
Rosemary <rosemarycadams@gmail.com>
2018-04-18 11:28:16 -0700

    post casting dialog

--- clients/frontend/src/SetRehearsals.js
+++ clients/frontend/src/SetRehearsals.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import Button from 'material-ui/RaisedButton';
-
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 import RehearsalRow from './RehearsalRow';
 
 class SetRehearsals extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      numRehearsals : 2
+      numRehearsals : 2,
+      open : false
     }
   };
 
   postCasting = () => {
     console.log("post")
+    this.setState({open: false});
   }
+
+  handleOpen = () => {
+    this.setState({open: true});
+  }
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
   
   addRehearsal = () => {
     let newRehearsalNum = this.state.numRehearsals
@@ -50,11 +61,35 @@ class SetRehearsals extends Component {
             <Button onClick={this.removeRehearsal}>Remove Rehearsal</Button>
           </div>
           <div className="postCasting">
-              <Button onClick={this.postCasting}>POST CASTING</Button>
+              <Button onClick={this.handleOpen}>POST CASTING</Button>
             </div>
           <div className="overlap">
           {/*This is where the overlapping availability will be displayed, same style as the one on the availability page, but we're not going to have that up for this*/}
           </div>
+          <Dialog
+            title="Confirm Casting"
+            actions={[
+              <FlatButton
+                label="Cancel"
+                primary={true}
+                onClick={this.handleClose}
+              />,
+              <FlatButton
+                label="Post Casting"
+                primary={true}
+                keyboardFocused={true}
+                onClick={this.postCasting}
+              />,
+            ]}
+            modal={false}
+            open={this.state.open}
+            onRequestClose={this.handleClose}
+          >
+            By clicking Post Casting you confirm that your selected cast is <strong>accurate</strong>, there are <strong>no conflicts</strong> with other choreographers, and that your rehearsal times are :
+            <br /> insert rehearsal times here <br />
+            <br />
+            <strong>An email will be sent to your cast with these times, and they will accept or decline their casting.</strong>
+          </Dialog>
         </div>
       </section>
   );
