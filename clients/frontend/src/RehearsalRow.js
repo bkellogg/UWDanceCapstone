9cca41264d7dd532cd7139e27646b9317249eebb
saniyamazman <mazmans@uw.edu>
2018-04-25 15:41:43 -0700

    push before pull

--- clients/frontend/src/RehearsalRow.js
+++ clients/frontend/src/RehearsalRow.js
@@ -9,21 +9,22 @@ import './styling/CastingFlowMobile.css';
 
 const styles = {
   customWidth: {
-    width: 140,
+    width: 130,
+    fontSize: 9
   },
 };
 
-const times = ["1000", "1030","1100","1130","1200","1230","1300", "1330", "1400","1430", 
-"1500", "1530", "1600", "1630", "1700", "1730", "1800", "1830", "1900", "1930", "2000", "2030", "2100"]
+const times = ["1000", "1030", "1100", "1130", "1200", "1230", "1300", "1330", "1400", "1430",
+  "1500", "1530", "1600", "1630", "1700", "1730", "1800", "1830", "1900", "1930", "2000", "2030", "2100"]
 
-const formattedTimes = ["10:00 AM", "10:30 AM","11:00 AM","11:30 AM","12:00 PM","12:30 PM","1:00 PM", "1:30 PM", "2:00 PM","2:30 PM", 
-"3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM", "7:00 PM", "8:30 PM", "9:00 PM", "9:30 PM", "10:00 PM"]
+const formattedTimes = ["10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM", "12:00 PM", "12:30 PM", "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM",
+  "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM", "7:00 PM", "8:30 PM", "9:00 PM", "9:30 PM", "10:00 PM"]
 
 class RehearsalRow extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rehearsal:{
+      rehearsal: {
         "day": "",
         "startTime": "",
         "endTime": ""
@@ -63,64 +64,74 @@ class RehearsalRow extends Component {
   }
 
   render() {
-      let timePicker = []
-      times.forEach((time, index) => {
-        return (
-            timePicker.push(
-                <MenuItem value={time} primaryText={formattedTimes[index]} />
-            ) 
+    let timePicker = []
+    times.forEach((time, index) => {
+      return (
+        timePicker.push(
+          <MenuItem value={time} primaryText={formattedTimes[index]} />
         )
-      })
-      let finished = this.props.finished
+      )
+    })
+    let finished = this.props.finished
     return (
       <section className="chooseRehearsalTimes">
-        <SelectField 
-          floatingLabelText="Day"
-          value={this.state.rehearsal.day}
-          style={styles.customWidth}
-          onChange={this.updateDay}
-          autoWidth={true}
-          disabled={finished}
-          className="pickDateTime"
-        >
-          <MenuItem value={"mon"} primaryText="Monday" />
-          <MenuItem value={"tues"} primaryText="Tuesday" />
-          <MenuItem value={"wed"} primaryText="Wednesday" />
-          <MenuItem value={"thurs"} primaryText="Thursday" />
-          <MenuItem value={"fri"} primaryText="Friday" />
-          <MenuItem value={"sat"} primaryText="Saturday" />
-          <MenuItem value={"sun"} primaryText="Sunday" />
+        <div className="singleRehearsal">
+          <div className="singleField">
+            <SelectField
+              floatingLabelText="Day"
+              value={this.state.rehearsal.day}
+              style={styles.customWidth}
+              onChange={this.updateDay}
+              autoWidth={true}
+              disabled={finished}
+              className="pickDateTime"
+            >
+              <MenuItem value={"mon"} primaryText="Monday" />
+              <MenuItem value={"tues"} primaryText="Tuesday" />
+              <MenuItem value={"wed"} primaryText="Wednesday" />
+              <MenuItem value={"thurs"} primaryText="Thursday" />
+              <MenuItem value={"fri"} primaryText="Friday" />
+              <MenuItem value={"sat"} primaryText="Saturday" />
+              <MenuItem value={"sun"} primaryText="Sunday" />
 
-        </SelectField>
-        <br/>
-        <SelectField
-          maxHeight={300}
-          style={styles.customWidth}
-          floatingLabelText="Start Time"
-          value={this.state.rehearsal.startTime}
-          onChange={this.updateStartTime}
-          autoWidth={true}
-          disabled={finished}
-        >
-          {timePicker}
+            </SelectField>
+          </div>
+          <br />
+          <div className="singleField">
+            <SelectField
+              className="selectField"
+              maxHeight={300}
+              style={styles.customWidth}
+              floatingLabelText="Start Time"
+              value={this.state.rehearsal.startTime}
+              onChange={this.updateStartTime}
+              autoWidth={true}
+              disabled={finished}
+            >
+              {timePicker}
 
-        </SelectField>
-        <br/>
-        <SelectField
-          maxHeight={300}
-          style={styles.customWidth}
-          floatingLabelText="End Time"
-          value={this.state.rehearsal.endTime}
-          onChange={this.updateEndTime}
-          autoWidth={true}
-          disabled={finished}
-        >
-          {timePicker}
+            </SelectField>
+          </div>
+          <br />
+          <div className="singleField">
+            <SelectField
+              className="selectField"
+              maxHeight={300}
+              style={styles.customWidth}
+              floatingLabelText="End Time"
+              value={this.state.rehearsal.endTime}
+              onChange={this.updateEndTime}
+              autoWidth={true}
+              disabled={finished}
+            >
+              {timePicker}
 
-        </SelectField>
+            </SelectField>
+          </div>
+        </div>
       </section>
-  );
-};
+    );
+  };
 
 }
 export default RehearsalRow;
