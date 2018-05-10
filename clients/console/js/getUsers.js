300ab265609c91a0fa76234c4f8c37267ec3b034
Nathan Swanson <swanson4@uw.edu>
2018-05-09 18:39:38 -0700

    worked on displaying user information

--- clients/console/js/getUsers.js
+++ clients/console/js/getUsers.js
@@ -1,24 +1,52 @@
 "use strict";
 
-const status = document.querySelector("#users")
-let auth = getAuth();
 
 refreshLocalUser();
 
+var page = 1;
+var allUsers = [];
+var done = false;
 
-$(document).ready(function () {
-    $('#users_table').dataTable({
-        "ajax": {
-            "url": API_URL_BASE + "users/all?page=1&includeInactive=true&auth=" + auth,
-            "dataSrc": "users"
-        },
-        "columns": [
-            { "data": "id"},
-            { "data": "firstName" },
-            { "data": "lastName" },
-            { "data": "role.displayName" },
-            { "data": "email" },
-            { "data": "active" }
-        ]
+getUsers();
+
+function getUsers() {
+    makeRequest("users/all?page=" + page + "&includeInactive=false", {}, "GET", true)
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return res.text().then((t) => Promise.reject(t));
+        })
+        .then((data) => {
+            $(data.users).each(function (index, value) {
+                var user = [];
+                user.push(value.id);
+                user.push(value.firstName);
+                user.push(value.lastName);
+                user.push(value.email);
+                user.push(value.role.displayName);
+                user.push("");
+                allUsers.push(user);
+            });
+        })
+        .then(() => {
+            populateUserTable(allUsers);
+        })
+}
+
+function populateUserTable(allUsers) {
+    var table = $('#users_table').DataTable({
+        "data": allUsers,
+        "columnDefs": [{
+            "targets": -1,
+            "data": null,
+            "defaultContent": "<button>Click!</button>"
+        }]
     });
-});
+
+    $('#users_table tbody').on('click', 'button', function () {
+        var data = table.row($(this).parents('tr')).data();
+        alert(data[0]);
+    });
+}
+
