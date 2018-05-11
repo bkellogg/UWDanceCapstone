40fe88020f4c80c8e0f810bc2192af0225fccbba
Brendan Kellogg <brendan6@uw.edu>
2018-05-11 00:59:20 -0700

    service workers can eat my asshole

--- servers/gateway/handlers/index.go
+++ servers/gateway/handlers/index.go
@@ -3,12 +3,19 @@ package handlers
 import (
 	"net/http"
 	"path"
+
+	"github.com/BKellogg/UWDanceCapstone/servers/gateway/appvars"
 )
 
 // IndexHandler handles the serving the webclient from the root resource.
 func IndexHandler(basePath string) func(w http.ResponseWriter, r *http.Request) {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, basePath+path.Base(r.URL.Path))
+		file := path.Base(r.URL.Path)
+		if file == "service-worker.js" {
+			w.Header().Set(appvars.HeaderContentType, "text/plain")
+			return
+		}
+		http.ServeFile(w, r, basePath+file)
 	}
 	return fn
 }
