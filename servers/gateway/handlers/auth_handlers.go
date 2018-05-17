9b2c3e8d4a24f0726bc8733495247c29e0a8a8f7
Brendan Kellogg <brendan6@uw.edu>
2018-05-17 10:36:29 -0700

    user signout

--- servers/gateway/handlers/auth_handlers.go
+++ servers/gateway/handlers/auth_handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/BKellogg/UWDanceCapstone/servers/gateway/appvars"
@@ -58,47 +59,64 @@ func (ctx *AuthContext) UserSignUpHandler(w http.ResponseWriter, r *http.Request
 
 // UserSignInHandler handles requests for user sign ins
 func (ctx *AuthContext) UserSignInHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
+	switch r.Method {
+	default:
 		http.Error(w, "requests to /api/v1/sessions must be POST", http.StatusMethodNotAllowed)
 		return
-	}
-	signInRequest := &models.SignInRequest{}
-	if err := receive(r, signInRequest); err != nil {
-		http.Error(w, "error decoding request: "+err.Message, err.Status)
-		return
-	}
+	case "POST":
+		signInRequest := &models.SignInRequest{}
+		if err := receive(r, signInRequest); err != nil {
+			http.Error(w, "error decoding request: "+err.Message, err.Status)
+			return
+		}
 
-	// Get the user from the database corresponding to the given email
-	user, dberr := ctx.store.GetUserByEmail(signInRequest.Email, true)
-	if dberr != nil {
-		http.Error(w, "error performing database lookup: "+dberr.Message, dberr.HTTPStatus)
-		return
-	}
-	if user == nil || !user.Active {
-		dummyAuthenticate()
-		http.Error(w, "invalid credentials", http.StatusUnauthorized)
-		return
-	}
+		// Get the user from the database corresponding to the given email
+		user, dberr := ctx.store.GetUserByEmail(signInRequest.Email, true)
+		if dberr != nil {
+			http.Error(w, "error performing database lookup: "+dberr.Message, dberr.HTTPStatus)
+			return
+		}
+		if user == nil || !user.Active {
+			dummyAuthenticate()
+			http.Error(w, "invalid credentials", http.StatusUnauthorized)
+			return
+		}
 
-	// Compare the passwords to check if they match. If they do, then the sign in is valid
-	// If they don't, then reject the signin request.
-	if err := user.Authenticate(signInRequest.Password); err != nil {
-		http.Error(w, err.Error(), http.StatusUnauthorized)
-		return
-	}
+		// Compare the passwords to check if they match. If they do, then the sign in is valid
+		// If they don't, then reject the signin request.
+		if err := user.Authenticate(signInRequest.Password); err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
 
-	state := sessions.NewSessionState(user)
-	if _, err := sessions.BeginSession(ctx.SessionKey, ctx.SessionsStore, state, w); err != nil {
-		http.Error(w, "error beginning session: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
+		state := sessions.NewSessionState(user)
+		if _, err := sessions.BeginSession(ctx.SessionKey, ctx.SessionsStore, state, w); err != nil {
+			http.Error(w, "error beginning session: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-	userResponse, err := ctx.permChecker.ConvertUserToUserResponse(user)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+		userResponse, err := ctx.permChecker.ConvertUserToUserResponse(user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 
-	respond(w, userResponse, http.StatusOK)
+		respond(w, userResponse, http.StatusOK)
+	case "DELETE":
+		_, err := sessions.EndSession(r, ctx.SessionKey, ctx.SessionsStore)
+		if err != nil {
+			status := http.StatusInternalServerError
+			if err == sessions.ErrInvalidScheme ||
+				err == sessions.ErrNoSessionID ||
+				err == sessions.ErrInvalidID {
+				status = http.StatusBadRequest
+			} else if err == sessions.ErrStateNotFound {
+				status = http.StatusNotFound
+			}
+			http.Error(w, fmt.Sprintf("error signing out: %v", err), status)
+			return
+		}
+		respondWithString(w, "you have been signed out", http.StatusOK)
+	}
 }
 
 // performs a dummy authentication to mimic a real authentication to
