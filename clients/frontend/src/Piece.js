eba240d1315ab6a3fefe7daf1da8546ccfccd5a7
saniyamazman <mazmans@uw.edu>
2018-05-25 14:04:34 -0700

    sign out fixedgit add .git add .

--- clients/frontend/src/Piece.js
+++ clients/frontend/src/Piece.js
@@ -442,6 +442,7 @@ class Piece extends Component {
 
                       <p><b>If music will be performed live, number of musicians: </b> </p>
                       <SelectField
+                        style={{backgroundColor: 'white', border: '1px solid lightgray', borderRadius: '5px', width: '90px', paddingLeft: '10px'}}
                         defaultValue={this.state.numMusicians}
                         value={this.state.numMusicians}
                         onChange={this.handleChangeMusician}
@@ -541,7 +542,7 @@ class Piece extends Component {
                 </section>
               }
             </div>
-            </section>
+            </section>  
             }
           </div>
         </div>
