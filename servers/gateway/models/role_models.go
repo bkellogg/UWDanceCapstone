f5194f5c93758e386d67486adafcee5778cc8eb0
Brendan Kellogg <brendan6@uw.edu>
2018-05-28 16:27:54 -0700

    role name and display name now properly trimmed

--- servers/gateway/models/role_models.go
+++ servers/gateway/models/role_models.go
@@ -40,11 +40,14 @@ func (nr *NewRole) Validate() error {
 	if len(nr.DisplayName) > appvars.MaxRoleDisplayNameLength {
 		return fmt.Errorf("role display name can only be %d characters long", appvars.MaxRoleDisplayNameLength)
 	}
+	nr.Name = strings.TrimSpace(nr.Name)
+	nr.DisplayName = strings.TrimSpace(nr.DisplayName)
 	nr.Name = strings.ToLower(strings.Replace(nr.Name, " ", "_", -1))
 
 	nr.DisplayName = strings.Title(strings.ToLower(nr.DisplayName))
 	if nr.Level < 1 || nr.Level > 100 {
 		return fmt.Errorf("role level must be between 1 and 100")
 	}
+
 	return nil
 }
